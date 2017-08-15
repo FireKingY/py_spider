08551ee93bee5d9f813aa8cc9c0da9d51df64da7
FireKingY <972023182@qq.com>
2017-08-15 16:05:16 +0800

    增加header 间隔时间

--- bilibili/bilibili/spiders/videoSpider.py
+++ bilibili/bilibili/spiders/videoSpider.py
@@ -3,12 +3,21 @@ import json
 from bilibili.items import VideoItem
 import csv
 import time
+import random
 
 class VideoSpider(scrapy.Spider):
 	name="video"
 	url=r'https://api.bilibili.com/archive_rank/getarchiverankbypartion?callback=jQuery17208522017968440501_1502731802625&tid=24&pn='
+	header={'Host': 'api.bilibili.com',
+			'Connection': 'keep-alive',
+			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+			'Upgrade-Insecure-Requests': '1',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Accept-Language': 'zh',
+			}
 	#start page
-	cpage=209
+	cpage=9240
 
 	def parse(self,response):
 		videos=json.loads(str(response.body,encoding='utf-8'))['data']['archives']
@@ -31,10 +40,10 @@ class VideoSpider(scrapy.Spider):
 			yield item
 
 		if count!=0:
-			time.sleep(3)
+			time.sleep(6+random.randint(0,4))
 			self.cpage+=1;
-			yield scrapy.Request(self.url+str(self.cpage))
+			yield scrapy.Request(self.url+str(self.cpage),headers=self.header)
 
 
 	def start_requests(self):
-		yield scrapy.Request(self.url+str(self.cpage))
+		yield scrapy.Request(self.url+str(self.cpage),headers=self.header)
